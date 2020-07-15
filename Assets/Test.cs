a1b23b7402e4b179bb889fd9d8691658dea11555
Taku951 <17itagaki@gmail.com>
2020-07-15 16:52:17 +0900

    ScripiTest プロジェクト　Lesson4 課題提出
    配列の降順昇順出力、Magic関数

---
+++ Assets/Test.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour {
+
+    //Bossクラス
+    public class Boss
+    {
+        //初期MP
+        private int mp = 53;
+
+        //魔法攻撃関数
+        public void Magic()
+        {
+            this.mp -= 5;
+            if (this.mp >= 5)
+            {
+                Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+            }
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない。");
+            }
+        }
+    }
+
+    void Start()
+    {
+        //Bossクラスの実体化
+        Boss magic = new Boss ();
+
+        //配列初期化
+        int[] array = { 0, 10, 20, 30, 40 };
+
+        //昇順表示
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //降順表示
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //魔法を十回繰り返す
+        for (int i = 0; i < 10; i++)
+        {
+            magic.Magic();
+        }
+        
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
